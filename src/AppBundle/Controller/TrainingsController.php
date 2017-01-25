54ac6c1cbbf0d03ad6025a40a01101a1cb19fc8e
sanchota <sancho@eboox.it>
2017-01-25 12:07:28 +0100

    ricerca training

--- src/AppBundle/Controller/TrainingsController.php
+++ src/AppBundle/Controller/TrainingsController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\Sport;
+use CrEOF\Spatial\PHP\Types\Geometry\Point;
 
 class TrainingsController extends FOSRestController
 {
@@ -30,11 +31,68 @@ class TrainingsController extends FOSRestController
 	// [GET] /trainings
 	// Set search parameters
     public function getTrainingsAction(){
-        $training = $this->getDoctrine()->getRepository('AppBundle:Training')->findAll();
-		if(!$training){
+
+		//Conversione degrees/meters (Earth mean radius)*PI/180
+		$conversion_factor = 88222;
+
+		//Find request parameters
+		$request = $this->getRequest();
+		
+		//The training is public or visible only to facebook users
+		$is_public = $request->get('is_public');
+		
+		//The user starting position to search for trainings
+		$x = $request->get('x');
+		$y = $request->get('y');
+		$point = new Point($x,$y);
+		
+		//The max distance in meters of training
+		$max_distance = $request->get('distance');
+
+		//The sports I intend to search within
+		$sports = $request->get('sports');
+
+		//The date of training
+		//0 = today
+		//1 = tomorrow
+		//etc...
+		$date = $request->get('date');
+
+		//Instantiate the repositiory		
+		$repository = $this->getDoctrine()->getRepository('AppBundle:Training');
+		
+		// Query creation with filters
+		$query = $repository->createQueryBuilder('t')
+
+					//Filtering conditions    	
+    				->where('t.is_public = :is_public')
+    				->andWhere('t.sport_id IN (:sports)')
+    				->andWhere('DATE_DIFF(t.start,CURRENT_DATE()) IN (:date)')
+    				->andWhere("st_distance(t.position,point(:x_position,:y_position))*:conversion_factor < :max_distance")
+
+					//Order by distance ASC
+    				->orderBy("st_distance(t.position,point(:x_position,:y_position))", 'ASC')
+
+					//Parameters passage					
+    				->setParameter('is_public', $is_public)
+
+					->setParameter('x_position', $point->getX())
+					->setParameter('y_position', $point->getY())
+					->setParameter('conversion_factor', $conversion_factor)
+					->setParameter('max_distance', $max_distance)
+
+					->setParameter('sports', $sports)
+					->setParameter('date', $date)
+					
+    				->getQuery()
+		;
+		
+		$trainings = $query->getResult();
+
+		if(!$trainings){
 			throw $this->createNotFoundException('No collection found');
 		}else{
-			$view = $this->view($training, 200);
+			$view = $this->view($trainings, 200);
         	return $this->handleView($view);
 		}
     }
