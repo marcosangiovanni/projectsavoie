8303c0219d77c589a08bd3b40ecbf88d22f8ca99
sanchota <sancho@eboox.it>
2017-01-31 11:55:05 +0100

    aggiunto ordinamento per start date

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -143,6 +143,7 @@ class User extends BaseUser
 	 * @ORM\OneToMany(targetEntity="Training", mappedBy="user", cascade={"remove"})
 	 * @SerializedName("created_trainings")
 	 * @Groups({"detail"})
+	 * @ORM\OrderBy({"start" = "DESC"})
 	 */
     private $trainings;
     
