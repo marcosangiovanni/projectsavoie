03ebe6ae605e89b14ff2e00473ec49565aef325c
sanchota <sancho@eboox.it>
2017-02-07 17:05:16 +0100

    corretto sport controller per update (PUT)

--- src/AppBundle/Controller/SportsController.php
+++ src/AppBundle/Controller/SportsController.php
@@ -3,7 +3,14 @@
 namespace AppBundle\Controller;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
+
 use AppBundle\Entity\Sport;
+use AppBundle\Form\SportType;
+
+use JMS\Serializer\SerializationContext;
+use JMS\Serializer\DeserializationContext;
+use JMS\Serializer\SerializerBuilder;
+use Symfony\Component\HttpFoundation\Response;
 
 class SportsController extends FOSRestController
 {
@@ -46,12 +53,46 @@ class SportsController extends FOSRestController
 
 	// "put_sports"             
 	// [PUT] /sports/{id}
-    public function putSportAction($slug)
-    {}
+    public function putSportAction($id){
+    	
+		//Get the sport to update
+		$sport = $this->getDoctrine()->getRepository('AppBundle:Sport')->find($id);
+		
+		//Request Object
+		$request = $this->getRequest();
+		
+		//Serialization data (serializer and context)
+		$context = SerializationContext::create()->setGroups(array('detail'))->enableMaxDepthChecks();
+		$deserialization_context = DeserializationContext::create()->setGroups(array('detail'))->enableMaxDepthChecks();
+		$serializer = SerializerBuilder::create()->build();
+
+		//Deserialized object with field conversion (see JMS Groups and SerializedName)
+		$obj = $serializer->deserialize($request->getContent(), 'AppBundle\Entity\Sport', 'json', $deserialization_context);
+		$objson = json_decode($serializer->serialize($obj, 'json'),true);
+		
+		//Form creation for update
+		$form = $this->createForm(SportType::class, $sport);
+		$form->bind($objson);
+		
+		//Check FORM
+		if($form->isValid()){
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($sport);
+		    $em->flush();
+		}
+		
+		/* SERIALIZATION */
+		$jsonContent = $serializer->serialize($sport, 'json', $context);
+		
+		/* JSON RESPONSE */
+		$jsonResponse = new Response($jsonContent);
+		return $jsonResponse->setStatusCode(200);
+		
+    }
 
 	// "delete_sports"          
 	// [DELETE] /sports/{id}
-    public function deleteSportAction($slug)
+    public function deleteSportAction($id)
     {}
 
 }
